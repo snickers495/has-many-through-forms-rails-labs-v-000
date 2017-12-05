fccd40d86ea99773d656397a411ac0d2957db6f3
Lisa Lee <snickers495@gmail.com>
2017-12-05 15:01:24 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,17 @@
 class PostsController < ApplicationController
-  def show
-    @post = Post.find(params[:id])
-  end
+    def show
+      @post = Post.find(params[:id])
+      @comment = @post.comments.build
+    end
 
-  def index
-    @posts = Post.all
-  end
+    def index
+      @posts = Post.all
+    end
 
-  def new
-    @post = Post.new
-  end
+    def new
+
+     @post = Post.new
+    end
 
   def create
     post = Post.create(post_params)
