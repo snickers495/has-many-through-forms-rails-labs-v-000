3b12a7ae83b6a0e2f48274e68fe53c43ab101d84
Lisa Lee <snickers495@gmail.com>
2017-12-05 14:53:04 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,9 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
-  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :user, reject_if: :all_blank
   has_many :comments
   has_many :users, through: :comments
 
-  def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.categories << category
-    end
-  end
+
 end
